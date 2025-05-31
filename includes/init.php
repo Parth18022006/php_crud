0e61b59d2df7a6a593e45e1e2d8de5553cfd00fa
Parth-Chavda <parth18chavda@gmail.com>
2025-05-31 17:45:58 +0530

    folder structure

--- includes/init.php
+++ includes/init.php
@@ -8,8 +8,8 @@ $db = "practice";
 $conn = new PDO("mysql:host=$host;dbname=$db",$name,$pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-define("BASE_DIR" , $_SERVER['DOCUMENT_ROOT'] . "/Basic_crud/Basic_crud");
-define("BASE_URL", "/Basic_crud/Basic_crud");
+define("BASE_DIR" , $_SERVER['DOCUMENT_ROOT'] . "/php_crud");
+define("BASE_URL", "/php_crud");
 
 function pathof($path){
     return BASE_DIR .  "/" . $path;
