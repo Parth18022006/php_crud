d83b5decae8d6b36536f341e1fa9339174469866
Parth-Chavda <parth18chavda@gmail.com>
2025-06-02 10:21:40 +0530

    index updated

--- index.php
+++ index.php
@@ -20,10 +20,12 @@ include pathof('includes/header.php');
     </div>
 
     <!-- Main Content -->
-    <div class="col-md-9 col-lg-10 content">
-      <h1 class="mb-4">Index Page</h1>
-      <a href="<?= urlof('pages/category/index.php'); ?>" class="btn btn-primary me-2">CATEGORY</a>
-      <a href="<?= urlof('pages/product/index.php'); ?>" class="btn btn-primary me-2">PRODUCT</a>
+    <div class="main-content">
+      <div class="col-md-9 col-lg-10 content">
+        <h1 class="mb-4">Index Page</h1>
+        <a href="<?= urlof('pages/category/index.php'); ?>" class="btn btn-primary me-2">CATEGORY</a>
+        <a href="<?= urlof('pages/product/index.php'); ?>" class="btn btn-primary me-2">PRODUCT</a>
+      </div>
     </div>
   </div>
 </body>
