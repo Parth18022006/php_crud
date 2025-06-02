f61e066fe163840f40fd5ef06ae62179243c60a5
Parth-Chavda <parth18chavda@gmail.com>
2025-06-02 10:34:18 +0530

    updated label

--- pages/product/update_product.php
+++ pages/product/update_product.php
@@ -31,7 +31,7 @@ $categories = $c_stmt->fetchAll(PDO::FETCH_ASSOC);
     <!-- Sidebar -->
     <div class="col-md-3 col-lg-2 sidebar d-flex flex-column align-items-start px-3">
         <div class="w-100 border-bottom pb-3 mb-3">
-            <h4 class="text-white text-center w-100">Product Management</h4>
+            <h4 class="text-white w-100">Product Management</h4>
         </div>
         <a href="<?= urlof("./index.php"); ?>">Index page</a>
         <a href="<?= urlof("./pages/category/index.php"); ?>">Category</a>
